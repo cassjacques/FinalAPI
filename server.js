62ce1b5e1cb8c20b16cdc46ce5ac9ca0482b5a2e
cassjacques <75631685+cassjacques@users.noreply.github.com>
2021-03-09 09:06:00 -0800

    built file uploader

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+////////////////////////////////////////////////////////////////////////////////////////////////
+const fileUpload = require('express-fileupload');
+////////////////////////////////////////////////////////////////////////////////////////////////
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config()
@@ -6,6 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 4020
 const routes = require('./routes');
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+app.use(fileUpload());
+////////////////////////////////////////////////////////////////////////////////////////////////
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -18,5 +24,19 @@ app.use('/api/v1/fits', routes.fits);
 app.use('/api/v1/soss', routes.soss);
 app.use('/api/v1/users', routes.users);
 app.use('/api/v1/auth', routes.auth);
-
+////////////////////////////////////////////////////////////////////////////////////////////////
+app.post('/upload', (req, res)=> {
+    if(req.files === null) {
+        return res.status(400).json({ msg: 'No file uploaded'});
+    }
+    const file = req.files.file;
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.json({ fileName: file.name, filePath: `/uploads/$file.name`});
+    })
+})
+////////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
